e4baa288432a32fd5d7c5afc1abb9712a9c76270
cx <28240021@qq.com>
2018-03-19 15:47:57 +0800

    移除show.html index.php

--- stuajax/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$con = mysqli_connect('localhost','root','','db_by');
-	$res = mysqli_query($con,'SELECT * FROM news');
-	$len = mysqli_num_rows($res);
-	$row = array();
-	for ($i = 0; $i < $len; $i++) {
-		array_push($row,mysqli_fetch_object($res));
-	}
-	echo json_encode($row);
